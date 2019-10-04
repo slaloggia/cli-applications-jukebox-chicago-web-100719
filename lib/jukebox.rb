2344408721a233ec19d8d62276bed4f2a9183bf6
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-10-04 19:38:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -42,18 +42,7 @@ def exit_jukebox
 end
 
 
-def run 
-  songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+def run(songs) 
   puts "Please enter a command:"
   first_command = gets.strip
   case first_command
